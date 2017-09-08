767bb04b9506646d62045a6e59076d5e3b0aced9
Colin McKean <ezikill@hotmail.com>
2017-09-08 19:05:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,8 @@ def prime?(number)
   return false if number == 0
   return false if number == 1
   squares = []
- (2..number).each { |num| squares << num*num }
+  buchOfNums =  (2..number)
+  buchOfNums.each { |num| squares << num*num }
  squares.each do |x|
    if number % x == 0 && number != x
      puts "#{x} #{number}"
