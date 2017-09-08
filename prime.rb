8f079080629672a09b5c095ef83f169b6ba80647
Colin McKean <ezikill@hotmail.com>
2017-09-08 19:08:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number)
-  return false if number == 0
+  return false if number <= 0
   return false if number == 1
   (2..number).each do |x|
    if number % x == 0 && number != x
