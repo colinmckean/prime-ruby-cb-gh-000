df5c828d363abe392415393af5298964c66cb62f
Colin McKean <ezikill@hotmail.com>
2017-09-08 19:09:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+  return false if number <= 0
+  return false if number == 1
+  (2..number).each do |x|
+   if number % x == 0 && number != x
+     return false
+   end
+ end
+ true
+end
