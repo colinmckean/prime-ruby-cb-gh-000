3d8de79727123dcebc36ebf5860a49146ffe163f
Colin McKean <ezikill@hotmail.com>
2017-09-08 18:49:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
   squares = []
  (1...number).each { |num| squares << num*num }
  squares.each do |x|
-   if number % x != 0
+   if number % x == 0
      return true
    end
  end
