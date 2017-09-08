9eaf84bf953abdb3ce177f958add1b25876c472e
Colin McKean <ezikill@hotmail.com>
2017-09-08 19:08:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,7 @@
 def prime?(number)
   return false if number == 0
   return false if number == 1
-  buchOfNums = (2..number)
- buchOfNums.each do |x|
+  (2..number).each do |x|
    if number % x == 0 && number != x
      return false
    end
